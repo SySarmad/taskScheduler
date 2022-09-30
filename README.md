a9a2fa3b4f82ce13e8de7b90cc73b14ff3d7c30c
sysarmad15 <sarmad@grapheene.net>
2022-09-29 23:52:49 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Fauna Task Scheduler
+
+### Usage
+```bash
+npm i 
+```
+
+```bash
+npm run build
+cat [path-to-file] > node ./dist/index.js
+```
+
+**Problem:**
+ Task execution ordering. Imagine a system of related tasks where
+ some tasks depend on others to complete before they can start. Some tasks
+ will have no dependencies and can start right away, and others will need
+ to wait for one or more tasks to complete.
+
+ These dependency relationships are defined in a text stream with lines
+ starting with a task identifier (1-20 case sensitive roman alphabet
+ characters [a-zA-Z]), followed by a ':', followed by a comma delimited list
+ of dependencies (task identifiers). For example, if T depends on A and B,
+ you'll have the line:
+
+ T:A,B
+
+This file is valid input, and contains four examples of task groups
+
+**EXAMPLE OUTPUT(S)**
+B A T 
+
+A B T
+
+
+
+
+## Betterments
+
+If i had more time or in an ideal world, I would like to experiment with multiple data structures. I think we are loosing some efficiency in the current iteration, wanted to go with what I know out of the gate. The light memoization helps, but I worry about some edge cases here.
---
+++ src/index.ts
@@ -0,0 +1,112 @@
+import { createInterface } from 'readline'
+
+type Node = {
+    name: string,
+    parent: Node | undefined,
+    children: Node[]
+}
+
+class Tree {
+
+    private cache = new Map<string, Node>()
+    private scheduled = new Map<string, boolean>()
+    constructor() { }
+
+    addTask(name: string, deps: string[]): void {
+        if (!this.cache.has(name)) {
+            this.cache.set(name, this.createNode(name))
+        }
+        let parent = this.cache.get(name)
+        for (let dep of deps) {
+            let childName = dep.trim().replace(' ', '')
+            let child = this.cache.has(childName) ? this.cache.get(childName) : this.createNode(childName, parent)
+            if (child != undefined) {
+                this.cache.set(childName, child)
+                parent?.children.push(child)
+            }
+        }
+    }
+
+    compute(): string {
+        for (let key of this.cache.keys()) {
+            const node = this.cache.get(key)
+            if(node !== undefined){
+                this.helper(node)
+            }
+            
+        }
+        let order = '';
+        for (let key of this.scheduled.keys()) {
+            order += key + ' '
+        }
+
+        return order
+    }
+
+    correctFormat(str: string): Boolean {
+        if (str.includes(':')) {
+            let split = str.split(':')
+            if (RegExp(/^\p{L}/, 'u').test(split[0])) {
+                return true;
+            }
+            return false;
+        }
+        return false
+    }
+
+    isEmpty(): Boolean {
+        return (this.cache.size === 0)
+    }
+
+    private createNode(name: string, parent: Node | undefined = undefined, children: Node[] = new Array<Node>()): Node {
+        return {
+            name: name,
+            parent: parent,
+            children: children
+        }
+    }
+
+    private helper(node: Node) {
+        if(this.scheduled.has(node.name)) {return}
+        if (node.children.length > 0) {
+            
+            for (let child of node.children) {
+                this.helper(child)
+            }
+        }
+        if (!this.scheduled.has(node.name)) {
+            this.scheduled.set(node.name, true)
+        }
+    }
+}
+
+async function main() {
+
+    const rl = createInterface({
+        input: process.stdin,
+        //output: process.stdout
+    });
+
+    let tree = new Tree()
+    for await (const line of rl) {
+        if (line.trim()[0] === '#' || line.length === 0) {
+            if (!tree.isEmpty()) {
+                console.log(tree.compute())
+                tree = new Tree()
+            }
+            continue
+        }
+        if (!tree.correctFormat(line)) {
+            throw new Error(`Line is incorrect format: ${line}`)
+        }
+        const task = line.trim().split(':')
+        tree.addTask(task[0], task[1].length > 0 ? task[1].split(',') : new Array<string>())
+    }
+    console.log(tree.compute())
+
+}
+
+main().then(() => process.exit(0)).catch(e => {
+    console.error(e)
+    process.exit(1)
+})
