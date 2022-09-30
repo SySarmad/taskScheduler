15b04d29c93c3a6e7dee3d733922befbc65fca3f
sysarmad15 <sarmad@grapheene.net>
2022-09-30 00:15:20 -0700

    cleaning it up

--- src/index.ts
+++ src/index.ts
@@ -12,52 +12,6 @@ class Tree {
     private scheduled = new Map<string, boolean>()
     constructor() { }
 
-    addTask(name: string, deps: string[]): void {
-        if (!this.cache.has(name)) {
-            this.cache.set(name, this.createNode(name))
-        }
-        let parent = this.cache.get(name)
-        for (let dep of deps) {
-            let childName = dep.trim().replace(' ', '')
-            let child = this.cache.has(childName) ? this.cache.get(childName) : this.createNode(childName, parent)
-            if (child != undefined) {
-                this.cache.set(childName, child)
-                parent?.children.push(child)
-            }
-        }
-    }
-
-    compute(): string {
-        for (let key of this.cache.keys()) {
-            const node = this.cache.get(key)
-            if(node !== undefined){
-                this.helper(node)
-            }
-            
-        }
-        let order = '';
-        for (let key of this.scheduled.keys()) {
-            order += key + ' '
-        }
-
-        return order
-    }
-
-    correctFormat(str: string): Boolean {
-        if (str.includes(':')) {
-            let split = str.split(':')
-            if (RegExp(/^\p{L}/, 'u').test(split[0])) {
-                return true;
-            }
-            return false;
-        }
-        return false
-    }
-
-    isEmpty(): Boolean {
-        return (this.cache.size === 0)
-    }
-
     private createNode(name: string, parent: Node | undefined = undefined, children: Node[] = new Array<Node>()): Node {
         return {
             name: name,
@@ -77,6 +31,65 @@ class Tree {
         if (!this.scheduled.has(node.name)) {
             this.scheduled.set(node.name, true)
         }
+    }
+
+    /* 
+        it works, i might want to spend some time optimizing 
+        both of these function the data structure itself i think would 
+        be prone to breaking down on the right edge case,
+        going for quick and dirty a bit here. 
+    */
+
+    addTask(name: string, deps: string[]): void {
+        if (!this.cache.has(name)) {
+            this.cache.set(name, this.createNode(name))
+        }
+        let parent = this.cache.get(name)
+        for (let dep of deps) {
+            let childName = dep.trim().replace(' ', '')
+            let child = this.cache.has(childName) ? this.cache.get(childName) : this.createNode(childName, parent)
+            if (child != undefined) {
+                this.cache.set(childName, child)
+                parent?.children.push(child)
+            }
+        }
+    }
+
+    
+    compute(): string {
+        for (let key of this.cache.keys()) {
+            const node = this.cache.get(key)
+            if(node !== undefined){
+                this.helper(node)
+            }
+            
+        }
+        let order = '';
+        for (let key of this.scheduled.keys()) {
+            order += key + ' '
+        }
+
+        return order
+    }
+
+    /* 
+        hate this validation function, probably wouldnt use regex at all in production and instead go for a tokenization 
+        but also do it all in one step may down below in the 'main' function. But wanted to show i at least thought about it
+        
+    */
+    correctFormat(str: string): Boolean {
+        if (str.includes(':')) {
+            let split = str.split(':')
+            if (RegExp(/^\p{L}/, 'u').test(split[0])) {
+                return true;
+            }
+            return false;
+        }
+        return false
+    }
+
+    isEmpty(): Boolean {
+        return (this.cache.size === 0)
     }
 }
 
